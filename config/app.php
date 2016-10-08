f6261ebf469cad5ad5e1cade09a76e360a0a9398
lokko13 <carlo.carabeo@gmail.com>
2016-10-08 18:41:53 +0800

    add cartalyst

--- config/app.php
+++ config/app.php
@@ -166,8 +166,8 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
-        //
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
+        
 
         /*
          * Application Service Providers...
@@ -229,8 +229,9 @@ return [
         /*
         Package Facades
         */
-        'Debugbar' => Barryvdh\Debugbar\Facade::class
-
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
     ],
 
 ];
