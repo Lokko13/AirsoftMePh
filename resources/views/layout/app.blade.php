2ead9c6e2621f9eb940621e7871f8e19e832608b
lokko13 <carlo.carabeo@gmail.com>
2016-10-10 09:48:06 +0800

    auth stuff

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title', 'AirsoftMe')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="#" rel="stylesheet" type="text/css">
+        @yield('styles')
+        
+    </head>
+    <body>
+        @yield('body')
+
+        @yield('footer-scripts')
+    </body>
+</html>
