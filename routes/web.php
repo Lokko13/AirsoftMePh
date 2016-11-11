9ccf5cb33f6361b2a06f222413063f29774333bb
Carlo Carabeo <Carlo Carabeo>
2016-11-11 15:21:10 +0800

    clean

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(array('prefix' => 'auth'), function(){
     Route::group(array('middleware' => 'guest'), function(){
-        Route::get('login', 'AuthController@getLogin')->name('login');
-        Route::get('register', 'AuthController@getRegister')->name('register');
-        Route::post('login', 'AuthController@postLogin')->name('auth.login');
-        Route::post('register', 'AuthController@postRegister')->name('auth.register');
-        Route::get('activation/complete', 'AuthController@completeActivation')->name('auth.activation.complete');
-        Route::get('activation/new/{user_id?}', 'AuthController@getActivation')->name('auth.activation.new');
+        Route::get('login', 'Auth\AuthController@getLogin')->name('login');
+        Route::get('register', 'Auth\AuthController@getRegister')->name('register');
+        Route::post('login', 'Auth\AuthController@postLogin')->name('auth.login');
+        Route::post('register', 'Auth\AuthController@postRegister')->name('auth.register');
+        Route::get('activation/complete', 'Auth\AuthController@completeActivation')->name('auth.activation.complete');
+        Route::get('activation/new/{user_id?}', 'Auth\AuthController@getActivation')->name('auth.activation.new');
     });
 
-    Route::get('logout', 'AuthController@logout')->name('logout');
+    Route::get('logout', 'Auth\AuthController@logout')->name('logout');
 });
